d1c4007da82a474cee177d2e0383c189a4ac1b59
Hiếu Hồ <fantastichaha11@gmail.com>
2024-08-08 16:07:52 +0700

    add updateUserPassword in userModel

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -38,6 +38,19 @@ class userModel {
             throw new Error(`Error finding user by email: ${error.message}`);
         }
     }
+
+    async updateUserPassword(email, hash) {
+        const query = `
+        UPDATE users SET password = $1 WHERE email = $2
+        RETURNING *
+        `;
+        try {
+            const result = await pool.query(query, [hash, email]);
+            return result.rows[0];
+        } catch (error) {
+            throw new Error(`Error updating user password: ${error.message}`);
+        }
+    }
 };
 
 export default new userModel();
